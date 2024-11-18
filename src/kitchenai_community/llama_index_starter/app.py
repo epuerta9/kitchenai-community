69dba54e1bed4b5678f7495a829a652514b78e7f
epuerta <esteban_puerta@rhinosearch.io>
2024-11-17 20:50:17 -0600

    rm chroma_db; starter app.py with embed

--- src/kitchenai_community/llama_index_starter/app.py
+++ src/kitchenai_community/llama_index_starter/app.py
@@ -1,9 +1,8 @@
-from ninja import Schema
 from kitchenai.contrib.kitchenai_sdk.kitchenai import KitchenAIApp
 
 from llama_index.core import VectorStoreIndex, StorageContext
 from llama_index.vector_stores.chroma import ChromaVectorStore
-
+from kitchenai.contrib.kitchenai_sdk.api import QuerySchema, EmbedSchema
 from llama_index.llms.openai import OpenAI
 import os 
 import chromadb
@@ -14,25 +13,24 @@ from llama_index.core.extractors import (
     TitleExtractor,
     QuestionsAnsweredExtractor)
 
+from llama_index.core import Document
 
 # create client and a new collection
 chroma_client = chromadb.PersistentClient(path="chroma_db")
-chroma_collection = chroma_client.create_collection("quickstart")
+chroma_collection = chroma_client.get_or_create_collection("quickstart")
 llm = OpenAI(model="gpt-4")
-
-
-class Query(Schema):
-    query: str
 
 kitchen = KitchenAIApp()
 
+import logging
+
+logger = logging.getLogger(__name__)
 
 @kitchen.storage("file")
 def chromadb_storage(dir: str, metadata: dict = {}, *args, **kwargs):
     """
     Store uploaded files into a vector store with metadata
     """
-    chroma_collection = chroma_client.get_or_create_collection("quickstart")
     parser = Parser(api_key=os.environ.get("LLAMA_CLOUD_API_KEY", None))
 
     response = parser.load(dir, metadata=metadata, **kwargs)
@@ -51,15 +49,29 @@ def chromadb_storage(dir: str, metadata: dict = {}, *args, **kwargs):
     return {"msg": "ok", "documents": len(response["documents"])}
 
 
-@kitchen.query("query")
-async def query(request, query: Query):
+@kitchen.embed("embed")
+def embed(instance, metadata: dict = {}):
+    """Embed single pieces of text"""
+    documents = [Document(text=instance.text)]
+    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+            
+    VectorStoreIndex.from_documents(
+        documents, storage_context=storage_context, show_progress=True,
+            transformations=[TokenTextSplitter(), TitleExtractor(),QuestionsAnsweredExtractor()]
+    )
+    return "ok"
+
+
+@kitchen.query("simple-query")
+def query(request, data: QuerySchema):
     vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
 
     index = VectorStoreIndex.from_vector_store(
         vector_store,
     )
-
-    chat_engine = index.as_chat_engine(chat_mode="best", llm=llm, verbose=True)
-    response = await chat_engine.achat(query.query)
+    query_engine = index.as_query_engine(chat_mode="best", llm=llm, verbose=True)
+    response = query_engine.aquery(data.query)
 
     return {"msg": response.response}
